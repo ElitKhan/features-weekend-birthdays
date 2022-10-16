df974f53744b88fa943dd611778f5ab43a410518
ElitKhan <47053938+ElitKhan@users.noreply.github.com>
2022-10-16 17:45:41 +0100

    Add files via upload

---
+++ birthdays.js
@@ -0,0 +1,27 @@
+function run(birth_day, from = 2016, to = 2065) {
+    const [day, month] = birth_day.split('-');
+    let date = "";
+    let features_dates = [];
+    let whichDay = -1;
+    let increment = (Number(month) === 2 && Number(day) === 29) ? 4 : 1
+    while (from <= to) {
+        date = `${from}-${month}-${day}`
+        whichDay = new Date(date).getDay();
+        getBirthday(whichDay, from) && features_dates.push(getBirthday(whichDay, from))
+        from += increment;
+    }
+    return features_dates.join(' ')
+}
+
+function getBirthday(day, year) {
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    if (day === 0) {
+        return days[day] + '-' + year.toString();
+    } else if (day === 5) {
+        return days[day] + '-' + year.toString();
+    } else if (day === 6) {
+        return days[day] + '-' + year.toString();
+    }
+}
+
+console.log(run('28-02'))
